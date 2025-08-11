d361e8732c42b70788ef523df10e2eea3792a3fd
Suman Kundu <kundu7878@gmail.com>
2025-08-11 10:36:24 +0530

    Update ColorData.h

--- ColorData.h
+++ ColorData.h
@@ -4,13 +4,8 @@
 
 namespace TelCoColorCoder
 {
-    // External declarations for color name arrays
-    extern const char* const MajorColorNames[];
-    extern const int numberOfMajorColors;
-    extern const char* const MinorColorNames[];
-    extern const int numberOfMinorColors;
-
     // Function declarations for color-number conversion
     ColorPair GetColorFromPairNumber(int pairNumber);
     int GetPairNumberFromColor(MajorColor major, MinorColor minor);
 }
+
