56cf25d67cab159542ea4bf257cee5698d756394
Suman Kundu <kundu7878@gmail.com>
2025-08-11 12:02:23 +0530

    Add files via upload

---
+++ ColorCoderTests.cpp
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+#include "ColorData.h"
+#include "ColorCodingManual.h"
+
+namespace TelCoColorCoder 
+{
+    TEST(ColorCoderTest, TestColorMapper) 
+    {
+        ColorPair colorPair = GetColorFromPairNumber(4);
+        EXPECT_EQ(colorPair.getMajor(), WHITE);
+        EXPECT_EQ(colorPair.getMinor(), BROWN);
+        
+        EXPECT_EQ(GetPairNumberFromColor(BLACK, ORANGE), 12);
+    }
+
+    TEST(ColorCoderTest, TestReferenceManual) 
+    {
+        std::string manual = GenerateReferenceManual();
+        EXPECT_TRUE(manual.find("| Pair | Major Color | Minor Color |") != std::string::npos);
+        EXPECT_TRUE(manual.find("White") != std::string::npos);
+        EXPECT_TRUE(manual.find("Violet") != std::string::npos);
+    }
+}
