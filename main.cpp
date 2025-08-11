d255f0d10dc2f215dee6a9b25cf8b2a1f6831c5a
Suman Kundu <kundu7878@gmail.com>
2025-08-11 12:20:14 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "ColorPair.h"
 #include "ColorCodingManual.h"
+#include "ColorCoderTests.h"
 
 int main() {
     // Print the color coding reference manual
     std::cout << TelCoColorCoder::FormatColorCodingManual() << std::endl;
+
+    TestColorMapper();
+    TestColorCodingManual();
+
+    std::cout << "All tests passed successfully!\n";
 
     return 0;
 }
